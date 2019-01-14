2177c6a2aed70fbe3235549c6fd9be2eb6ae743d
harkiratdhanoa <32875958+harkiratdhanoa@users.noreply.github.com>
2019-01-14 20:19:26 +0530

    Add files via upload

--- sigmoid.cpp
+++ sigmoid.cpp
@@ -2,6 +2,9 @@
 using std::cout;
 using std::vector;
 
+typedef vector<float> row;
+typedef vector<row> Matrix;
+
 
 vector<float> sigmoid(vector<float> v){
 	int i;
@@ -10,6 +13,7 @@ vector<float> sigmoid(vector<float> v){
 		v_out.push_back(1.0/(1.0+exp(-v[i])));
 	return v_out;
 }
+
 
 vector<float> softmax(vector<float> v){
     float sum=0.0;
@@ -25,13 +29,97 @@ vector<float> softmax(vector<float> v){
 }
 
 
-//int main(){
+Matrix maxPooling(Matrix image, int stride){
+    
+    if(image[0].size()%stride!=0)                                             // side padding
+        for(int i = 0; i<image.size();i++)
+            while(image[i].size()%stride!=0)
+                image[i].push_back(0.0);
+    if(image.size()%stride!=0){                                              // bottom padding
+        while(image.size()%stride!=0){
+            row r;
+            image.push_back(r);
+            for(int i = 0; i < image[0].size(); i++)
+                image[image.size()][i]=0.0;
+        }
+    }
+    Matrix maxpooled;
+    
+    for(int i = 0; i<image.size()/stride;i++){
+        row r;
+        maxpooled.push_back(r);
+         for(int j = 0; i<image[0].size();i++){
+             maxpooled[i].push_back(image[i][j]);
+              for(int t = 0; t<stride;t++)
+                  for(int s = 0; s<stride;s++)
+                      if(maxpooled[i][j]>image[i+t][j+s])
+                          maxpooled[i][j]=image[i+t][j+s];
+        }
+    }
+    return maxpooled;
+}
+
+
+Matrix avgPooling(Matrix image, int stride){
+    
+    if(image[0].size()%stride!=0)                                             // side padding
+        for(int i = 0; i<image.size();i++)
+            while(image[i].size()%stride!=0)
+                image[i].push_back(0.0);
+    if(image.size()%stride!=0){                                              // bottom padding
+        row r;
+        image.push_back(r);
+        for(int i = 0; i<image[0].size();i++)
+            while(image.size()%stride!=0)
+                image[image.size()][i]=0.0;
+    }
+    Matrix avgpooled;
+    
+    for(int i = 0; i<image.size()/stride;i++){
+        row r;
+        avgpooled.push_back(r);
+        for(int j = 0; i<image[0].size();i++){
+            avgpooled[i].push_back(0.0);
+            for(int t = 0; t<stride;t++)
+                for(int s = 0; s<stride;s++)
+                    avgpooled[i][j]+=image[i+t][j+s];
+        }
+    }
+    return avgpooled;
+}
+
+
+int main(){
 //    vector<float> a,b;
 //    a.push_back(1.0);
 //    a.push_back(1);
 //    a.push_back(0);
 //    b=softmax(a);
-//    for(int i=0;i<b.size();i++){
-//        cout << b[i] << " ";
-//    }
-//}
+    Matrix image;
+        row vect1;
+        vect1.push_back(10.0);
+        vect1.push_back(20.0);
+        vect1.push_back(30.0);
+        image.push_back(vect1);
+        row vect2;
+        vect2.push_back(40.0);
+        vect2.push_back(50.0);
+        vect2.push_back(60.0);
+        image.push_back(vect2);
+        row vect3;
+        vect3.push_back(70.0);
+        vect3.push_back(80.0);
+        vect3.push_back(90.0);
+        image.push_back(vect3);
+    
+    cout<<"hii";
+    Matrix outt = maxPooling(image,2);
+    
+    for(int i=0;i<outt.size();i++){
+        for(int j=0;j<outt[0].size();j++)
+        cout << outt[i][j] << " ";
+        cout << "\n";
+    }
+}
+
+
